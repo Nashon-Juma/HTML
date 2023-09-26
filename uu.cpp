5eade5eb9e2d4e8eea8ccc349a3ccc4c6c88af71
Nashon-Juma <93912358+Nashon-Juma@users.noreply.github.com>
2023-09-26 18:35:54 +0300

    Add files via upload

---
+++ uu.cpp
@@ -0,0 +1,87 @@
+#include<stdio.h>
+<!DOCTYPE html>
+ <head>
+    <title>this tables</title>
+</head>
+<body style="background-color: rgb(193, 219, 132);">
+    <form action="http://wwwn.php.com">
+        <p>nashon:
+         <input 
+         type="text" 
+         name="username" 
+         size="15"
+         maxlength="30" />
+        
+
+          </br><s>
+         thtthtth;
+         </s><input type="text" 
+            name="username" 
+            size="15"   
+            maxlength="30" />
+         
+        </p>
+        <p>juma:
+         <input type="password" name="password" size="15"
+         maxlength="30" />
+        </p>
+        <p>subscribe:
+            <input type="submit" name="subscibe" size="12"/>
+        </p>  
+
+
+    </form>
+         <form action="http://www.example.com/comments.php">
+         <p>What did you think of Nashons website?</p>
+         <textarea name="comments" cols="20" rows="4">Enter 
+            your comments...</textarea>
+    </form
+
+
+    <p>She had spent the last ten years in this room, 
+         sitting under a poster with an Oscar Wilde quote 
+         proclaiming that <q>Work is the refuge of 
+         people who have nothing better to do</q>. Although 
+         many considered her pioneering work, unraveling 
+         the secrets of the llama <abbr 
+         title="Nashon The Man">NTM</abbr>, to be an 
+         outstanding achievement, Molly <em>did</em> think 
+         she had something better to do.
+    </p>
+
+
+
+    <ol style="float:right;">
+         <li>Mousses</li>
+         <li>Pastries</li>
+             <ul>
+                 <li>Croissant</li>
+                 <li>Mille-feuille</li>
+                 <li>Palmier</li>
+                 <li>Profiterole</li>
+             </ul>
+         <li>Tarts</li>
+    </ol>
+
+
+    <a href="http://www.imdb.com" target="_blank">IMDB</a>
+
+
+    <p>
+        Movie Reviews:
+         <ul>
+             <li><a href="http://www.empireonline.com">Nashon</a></li>
+             <li><a href="http://www.metacritic.com">Caspa</a></li>
+             <li><a href="http://www.rottentomatoes.com">Nez</a></li>
+             <li><a href="http://www.variety.com">Jum</a></li>
+         </ul>
+    </p>
+
+    <a href="mailto:nashonhhh@gmail.com"target="_blank"method="post">Email Nashon</a>
+   <form action="http://www.example.com/comments.php">
+<p>What did you think of this gig?</p>
+ <textarea name="comments" cols="20" rows="4">Enter 
+ your comments...</textarea>
+</form>
+</body>
+</html>
